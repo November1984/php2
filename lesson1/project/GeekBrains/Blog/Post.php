8995e137a713d2cfbcb0d57c2915c18641f0ba9a
November.84 <november.mcmlxxxiv@icloud.com>
2024-02-22 10:53:29 +0300

    Урок 1 часть 1 done

---
+++ lesson1/project/GeekBrains/Blog/Post.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace GeekBrains\Blog;
+
+use \Person\Person;
+class Post {
+    public function __construct(
+        private int $id,
+        private Person $author,
+        private string $text
+    ){
+        $this->id = $id;
+        $this->author = $author;
+        $this->text = $text;
+    }
+
+    public function __toString()
+    {
+        return $this->author . " пишет: " . $this->text;
+    }
+}
---
+++ lesson1/project/cli.php
@@ -0,0 +1,23 @@
+<?php
+
+// require "Person/User.php";
+// require "Person/Name.php";
+// require "Person/Person.php";
+
+use Person\Person as Person;
+use Person\Name as Name;
+use GeekBrains\Blog\User as User;
+use GeekBrains\Blog\Post;
+
+spl_autoload_register("classLoad");
+function classLoad($className){
+    // echo "Person\\".$className.".php".PHP_EOL;
+   $s = str_replace("\\", DIRECTORY_SEPARATOR ,$className).".php";
+   require $s;
+}
+$user = new User(1, new Name ("Карлсон", null), "Helicopter");
+$person = new Person(new Name ("Карлсон", null), new DateTimeImmutable() );
+echo $user . PHP_EOL;
+echo $person . PHP_EOL;
+echo new Post(1, $person, "Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты.") . PHP_EOL;
+
